b7197c226b885bb9571851d15b186a2d9f143940
Jin Hu <jinhuca@gmail.com>
2025-09-20 21:22:00 -0400

    321

--- C01/C0101_Introduction/C0101_Introduction.cpp
+++ C01/C0101_Introduction/C0101_Introduction.cpp
@@ -9,7 +9,19 @@ struct MyStruct {
 MyStruct ms { 101, "Hello" };
 
 auto [u, v] = ms;
+auto [u1, v1] {MyStruct { 1,"world" }};
+auto [u2, v2](MyStruct { 12, "Month" });
+
+MyStruct getStruct() {
+  return MyStruct { 41, "hello" };
+}
+
+auto [id, val] = getStruct();
 
 int main() {
-  std::cout << u << ", " << v;
+  std::cout << u << ", " << v << '\n';
+  std::cout << u1 << ", " << v1 << '\n';
+  std::cout << u2 << ", " << v2 << '\n';
+
+  std::cout << id << ", " << val << '\n';
 }
